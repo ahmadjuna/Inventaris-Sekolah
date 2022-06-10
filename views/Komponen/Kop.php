5b4160c203a999271756f0e3ddb24c0cc221b6a1
ahmadjuna <ahmadjunaedi110600@gmail.com>
2022-06-10 12:43:25 +0700

    Tugas Web Lanjut

---
+++ views/Komponen/Kop.php
@@ -0,0 +1,20 @@
+<div style="border-bottom: solid;">
+    <div class="d-flex justify-content-start tes2 mt-3">
+        <div class="mx-auto align-self-center text-center">
+            <p class="m-0 p-0">
+                <b class="text-success" style="font-size: 20pt;">
+                    TUGAS WEB PEMROG LANJUT
+                </b>
+            </p>
+            <p class="m-0 p-0">
+                <b class="text-dark" style="font-size: 20pt;">
+                    AHMAD JUNAEDI
+                </b>
+            </p>
+            <p class="m-0 p-0">
+                <b class="text-dark" style="font-size: 20pt;">
+                201943501515 (R6S)
+                </b>
+            </p>
+    </div>
+</div>
